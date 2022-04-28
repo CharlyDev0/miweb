3c4a4f3901b5412303cec1b2e9e0d8957ae3f23f
CharlyDev0 <103946836+CharlyDev0@users.noreply.github.com>
2022-04-28 16:11:42 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,13 @@
+const a = ["alfa", "beta", "gama"];
+
+console.log(a);
+
+console.log(a[2]);
+
+const queen = ["Freddy", "Brian", "Jhon", "Roger"];
+
+console.log(queen[4]);
+
+queen.pop();
+queen.push("Stafeel");
+console.log(queen);
